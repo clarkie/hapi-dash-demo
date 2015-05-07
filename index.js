f8117b9c387301631affb44d2c9505ad90083603
Clarkie <clarkie@concrete.cc>
2015-05-07 15:36:27 +0100

    add a random route

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ var Hapi = require('hapi');
 var server = new Hapi.Server();
 server.connection({ port: 3334 });
 
+server.route({
+	method: 'get',
+	path: '/test',
+	handler: function (request, reply) {
+		return reply().code(400);
+	}
+});
+
 server.register({register: require('../hapi-dash')}, function (err) {
     if (err) {
         console.error('Failed to load plugin:', err);
