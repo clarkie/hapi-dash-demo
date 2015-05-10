74501c0930560b3226ae8f4a9241df92a3c7e6c2
Clarkie <clarkie@concrete.cc>
2015-05-10 11:58:21 +0100

    add a server inject on a random timer to stimulate the dashboard

--- index.js
+++ index.js
@@ -11,12 +11,26 @@ server.route({
 	}
 });
 
-server.register({register: require('../hapi-dash')}, function (err) {
-    if (err) {
-        console.error('Failed to load plugin:', err);
-    }
+server.register({register: require('hapi-dash')}, function (err) {
+	if (err) {
+		console.error('Failed to load plugin:', err);
+	}
 });
 
 server.start(function () {
-    console.log('Server running at:', server.info.uri);
+	console.log('Server running at:', server.info.uri);
 });
+
+// send some dummy data to the server.
+var sendRequest = function () {
+	server.inject('/', function (res) {
+	});
+};
+
+(function loop() {
+	var rand = Math.round(Math.random() * (2000 - 100)) + 100;
+	setTimeout(function() {
+			sendRequest();
+			loop();
+	}, rand);
+}());
